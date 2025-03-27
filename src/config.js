6944033ea0af495eb54d3971cd174c75d86a8836
kokistr <125700374+kokistr@users.noreply.github.com>
2025-03-27 12:00:56 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // src/config.js
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://tech0-gen-8-step4-dtx-poback-frhzgag5gubkb4gj.canadacentral-01.azurewebsites.net';
